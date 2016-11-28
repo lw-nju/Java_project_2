78bfb717cdb369e51ac4d25d973ae6f0b7c11616
Wei Liu <liuwei13cs@smail.nju.edu.cn>
2016-11-28 20:00:35 +0800

    确定Database_server的各个具体待实现方法

--- database_server.java
+++ database_server.java
@@ -1,5 +1,47 @@
 /**
  * Created by Liu on 2016/11/26.
  */
+
+import java.util.*;
+
 public class database_server {
+    public database_server()
+    {
+
+    }
+
+    public String get_like_num()           //从数据库获取点赞数目
+    {
+        return "";
+    }
+
+    public boolean update_like_num()        //更新数据库中信息
+    {
+        return true;
+    }
+
+    public String get_password()        //在数据库中获取密码
+    {
+        return "";
+    }
+
+    public boolean send_like_message()     //发送点赞数相关的信息
+    {
+        return true;
+    }
+
+    public boolean send_sign_message()
+    {
+        return true;
+    }
+
+    public void get_message()
+    {
+
+    }
+
+    public static void main(String args[])
+    {
+         new database_server();
+    }
 }
